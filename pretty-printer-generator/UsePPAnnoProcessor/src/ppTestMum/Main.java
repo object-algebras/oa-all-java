2c0190cf3c54d66ce30a47289a9f25482f658cf3
Xiang Ji <hi@xiangji.me>
2015-10-29 00:36:09 +0800

    Added test cases for Mumbler

--- pretty-printer-generator/UsePPAnnoProcessor/src/ppTestMum/Main.java
+++ pretty-printer-generator/UsePPAnnoProcessor/src/ppTestMum/Main.java
@@ -10,24 +10,73 @@ public class Main {
 	static <P, E> E makeBool(MumAlg<P, E> alg) {
 		return alg.booleanNode(false);
 	}
-	
-	static <P, E> E make2(MumAlg<P, E> alg) {
+
+	static <P, E> E makeLong(MumAlg<P, E> alg) {
+		return alg.longNode(123);
+	}
+
+	static <P, E> E makeIf(MumAlg<P, E> alg) {
 		return alg.ifNode(alg.booleanNode(true), alg.stringNode("It's true"), alg.stringNode("It's false"));
 	}
-	
-	static <P, E> E make3(MumAlg<P, E> alg) {
-		return alg.defineNode("a", alg.stringNode("variableA"));
+
+	static <P, E> E makeDefine(MumAlg<P, E> alg) {
+		return alg.defineNode("a", alg.stringNode("This variable refers to a string."));
+	}
+
+	static <P, E> E makeQuote(MumAlg<P, E> alg) {
+		return alg.quoteNode(alg.mumblerSymbol("a"));
+	}
+
+	static <P, E> E makeList(MumAlg<P, E> alg) {
+		ArrayList<E> list = new ArrayList<E>();
+		list.add(alg.stringNode("x"));
+		list.add(alg.longNode(6));
+
+		return alg.listNode(list);
+	}
+
+	static <P, E> E makeLambda(MumAlg<P, E> alg) {
+		ArrayList<E> argsList = new ArrayList<E>();
+		argsList.add(alg.mumblerSymbol("x"));
+
+		ArrayList<E> bodyList = new ArrayList<E>();
+		bodyList.add(alg.mumblerSymbol("*"));
+		bodyList.add(alg.mumblerSymbol("x"));
+		bodyList.add(alg.stringNode("x"));
+
+//		alg.listNode()
+		return alg.lambdaNode(argsList, bodyList);
+
+//		return alg.lambdaNode(listNode(argsList), listNode(bodyList));
+	}
+
+	static <P, E> E makeSymbol(MumAlg<P, E> alg) {
+		return alg.mumblerSymbol("x");
 	}
 
 	public static void main(String[] args) {
+		String boolTest = makeBool(new PPMumAlg());
+		System.out.println(boolTest);
 
-		String m1pp = makeBool(new PPMumAlg());
-		System.out.println(m1pp);
+		String longTest = makeLong(new PPMumAlg());
+		System.out.println(longTest);
 
-		String m2pp = make2(new PPMumAlg());
-		System.out.println(m2pp);
-		
-		String m3pp = make3(new PPMumAlg());
-		System.out.println(m3pp);
+		String ifTest = makeIf(new PPMumAlg());
+		System.out.println(ifTest);
+
+		String defineTest = makeDefine(new PPMumAlg());
+		System.out.println(defineTest);
+
+		String quoteTest = makeQuote(new PPMumAlg());
+		System.out.println(quoteTest);
+
+		String listTest = makeList(new PPMumAlg());
+		System.out.println(listTest);
+
+		String lambdaTest = makeLambda(new PPMumAlg());
+		System.out.println(lambdaTest);
+
+		String symbolTest = makeSymbol(new PPMumAlg());
+		System.out.println(symbolTest);
 	}
 }
